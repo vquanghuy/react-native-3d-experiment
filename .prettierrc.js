5ecc2039b0ef167cd9363ca7d0bdfc2c7f7225ea
Vu Quang Huy <vquanghuy@gmail.com>
2023-04-09 11:44:08 +0700

    Init blank project

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+};
